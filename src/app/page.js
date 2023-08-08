bfcd33e21194c9097fea902aa3900074353d812c
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-08 11:22:07 +0100

    ninth

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <div>
-    <form name="contact" >
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+    <h1>Contact us!</h1>
    </div>
   )
 }
