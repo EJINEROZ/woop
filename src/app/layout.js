c0b1ee6c04048030b820d776afd6c30eba51ae72
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-08 09:58:14 +0100

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <form method='post' name="contact" netlify > 
+      <input type="text" name="name" /> <br /> <br />
+      <input type="email" name="email" /> <br /> <br />
+      <textarea name="message"></textarea>
+    </form>
+    </body>
     </html>
   )
 }
