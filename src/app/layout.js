29e2c7b5092c45981377d14b24fc9017c1b3bafe
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-09 23:50:47 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,21 +12,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
+
+      <div class="bg-white p-8 rounded-lg shadow-md w-96">
+      <h2 className="text-2xl font-semibold mb-4">Contact us!</h2>
       <form name="contact" >
           <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+          <div class="mb-4">
+        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+        <input type="text" id="name" name="name" class="mt-1 p-2 border rounded-md w-full focus:ring focus:ring-blue-300" />
+      </div>
+      <div class="mb-4">
+        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+        <input type="email" id="email" name="email" class="mt-1 p-2 border rounded-md w-full focus:ring focus:ring-blue-300" />
+      </div>
+      <div class="mb-4">
+        <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+        <textarea id="message" name="message" rows="4" class="mt-1 p-2 border rounded-md w-full resize-none focus:ring focus:ring-blue-300"></textarea>
+      </div>
+      <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Submit</button>
+    </form>
+      </div>
     </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <div>
-    <h1>Contact us!</h1>
    </div>
   )
 }
